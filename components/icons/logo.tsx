cc8fdf5034f63cde24565b0337b78c38f3da2500
aymenyaseen <aimanggl@gmail.com>
2023-09-12 15:41:18 -0700

    logo-fix4

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function LogoIcon() {
-  return <Image src="logo.svg" height={24} width={24} alt="logo" />;
+  return <Image src="logo.svg" height={24} width={24} alt="Logo" />;
 }
