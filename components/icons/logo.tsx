3d91156474066e2a5859675feb9bc572396ac7c4
aymenyaseen <aimanggl@gmail.com>
2023-09-12 13:22:30 -0700

    fixing-logo

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -2,15 +2,25 @@ import clsx from 'clsx';
 
 export default function LogoIcon(props: React.ComponentProps<'svg'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-label={`${process.env.SITE_NAME} logo`}
-      viewBox="0 0 32 28"
-      {...props}
-      className={clsx('h-4 w-4 fill-black dark:fill-white', props.className)}
-    >
-      <path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z" />
-      <path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z" />
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px">
+      <g>
+        <path
+          d="M44.9,249.5h160.2c24.5,0,44.4-19.9,44.4-44.4V44.9c0-24.5-19.9-44.4-44.4-44.4H44.9
+     C20.4,0.5,0.5,20.4,0.5,44.9v160.2C0.5,229.6,20.4,249.5,44.9,249.5z"
+        />
+        <rect x="41.2" y="41.8" width="33.2" height="33.2" />
+        <rect x="41.3" y="98.6" width="33.2" height="46.5" />
+        <rect x="41.1" y="162.2" width="16.6" height="46.5" />
+        <rect x="175.5" y="185.5" width="33.2" height="23.2" />
+        <rect x="187.3" y="101" width="21.4" height="12.5" />
+        <rect x="177.9" y="135.5" width="30.8" height="25.2" />
+        <rect x="96.3" y="109.4" width="25.4" height="41.3" />
+        <rect x="138" y="93.6" width="19.9" height="31.5" />
+        <rect x="79.2" y="185.7" width="49.8" height="23.2" />
+        <rect x="148" y="200.1" width="14.8" height="8.7" />
+        <rect x="108.4" y="41.3" width="33.2" height="25.7" />
+        <rect x="175.5" y="41.3" width="33.2" height="46.5" />
+      </g>
     </svg>
   );
 }
