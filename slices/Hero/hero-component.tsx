1bd5b5ded293d9538da2d7a1e0d4698b68496e2a
skobaidze <sbkobaidze@gmail.com>
2023-10-17 00:31:10 +0400

    test without edge

--- slices/Hero/hero-component.tsx
+++ slices/Hero/hero-component.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { PrismicRichText } from '@prismicio/react';
-import Image from 'next/image';
 import { HeroSliceDefaultItem } from 'types.generated';
-import { asLink, asText } from '@prismicio/client';
+import { asLink } from '@prismicio/client';
+import { PrismicNextImage } from '@prismicio/next';
 
 import Link from 'next/link';
 
@@ -12,14 +12,15 @@ const HeroComponent = ({ data, className }: { data: HeroSliceDefaultItem; classN
     <div className={cn(className)}>
       <div className="relative h-full w-full ">
         {/*/@ts-ignore */}
-        <Image
-          src={asLink(data.image)!}
+        <PrismicNextImage
+          field={data.image}
           fill
           loading="eager"
           className="absolute"
-          alt={asText(data.title)}
+          // alt={asText(data.title)!}
           sizes="(min-width: 1024px) 66vw, 100vw"
         />
+
         <div className="absolute flex h-full w-full flex-col items-center justify-center  gap-3 text-white">
           <h3 className="text-xl font-bold lg:text-3xl">
             <PrismicRichText field={data.title} />
