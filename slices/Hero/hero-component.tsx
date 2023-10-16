e220601a5f8fd256f74ce0d59f7e8effef0193c4
skobaidze <sbkobaidze@gmail.com>
2023-10-17 00:06:45 +0400

    test without edge

--- slices/Hero/hero-component.tsx
+++ slices/Hero/hero-component.tsx
@@ -13,7 +13,7 @@ const HeroComponent = ({ data, className }: { data: HeroSliceDefaultItem; classN
       <div className="relative h-full w-full ">
         {/*/@ts-ignore */}
         <Image
-          src={data.image.url}
+          src={asLink(data.image)}
           fill
           className="absolute"
           alt={asText(data.title)}
