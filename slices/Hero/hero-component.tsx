e5077e8369c40b4757df31d32dd7c3e9e2833c65
skobaidze <sbkobaidze@gmail.com>
2023-10-17 01:18:07 +0400

    test

--- slices/Hero/hero-component.tsx
+++ slices/Hero/hero-component.tsx
@@ -4,20 +4,22 @@ import { cn } from '@/lib/utils';
 import { PrismicRichText } from '@prismicio/react';
 import { HeroSliceDefaultItem } from 'types.generated';
 import { asLink } from '@prismicio/client';
-import { PrismicNextImage } from '@prismicio/next';
+// import { PrismicNextImage } from '@prismicio/next';
+import Image from 'next/image';
 
 import Link from 'next/link';
 
 const HeroComponent = ({ data, className }: { data: HeroSliceDefaultItem; className: string }) => {
+  console.log(data.image);
   return (
     <div className={cn(className)}>
       <div className="relative h-full w-full ">
-        <PrismicNextImage
-          field={data.image}
+        <Image
+          src={data.image.url!}
           fill
           loading="eager"
           className="absolute"
-          // alt={asText(data.title)!}
+          alt={data.image.alt!}
         />
 
         <div className="absolute flex h-full w-full flex-col items-center justify-center  gap-3 text-white">
