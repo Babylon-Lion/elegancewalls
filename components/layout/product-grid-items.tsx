9b100b9c4454acfdeb058bbb1822c5ba4b824252
skobaidze <sbkobaidze@gmail.com>
2023-10-24 22:23:06 +0400

    test

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -8,19 +8,6 @@ export default function ProductGridItems({ products }: { products: Product[] }) 
       {products.map((product) => (
         <Grid.Item key={product.handle} className="animate-fadeIn lg:mb-5 2xl:mb-0">
           <ProductCard product={product} />
-          {/* <Link className="relative inline-block h-full w-full" href={`/product/${product.handle}`}>
-            <GridTileImage
-              alt={product.title}
-              label={{
-                title: product.title,
-                amount: product.priceRange.maxVariantPrice.amount,
-                currencyCode: product.priceRange.maxVariantPrice.currencyCode
-              }}
-              src={product.featuredImage?.url}
-              fill
-              sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
-            />
-          </Link> */}
         </Grid.Item>
       ))}
     </>
